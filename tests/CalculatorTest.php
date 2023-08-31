8d862182ffa925353ad9d0a6b8ad0cdd0d44d16c
asraf-mridha <asrafmridha@gmail.com>
2023-08-31 12:45:09 +0600

    Using DataProviders in PhpUnit Test Cases

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 // require("src/Calculator.php");
@@ -8,19 +9,35 @@ class CalculatorTest extends TestCase
 {
     private $calc;
 
-    protected function setUp() :void
+    protected function setUp(): void
     {
         $this->calc = new App\Calculator();
     }
 
     function testAddition()
     {
-        $result= $this->calc->add(2,3);
-        $this->assertEquals(5,$result);
+        $result = $this->calc->add(2, 3);
+        $this->assertEquals(5, $result);
     }
     function testMultiply(...$operands)
     {
         $result = $this->calc->multiply(5, 3);
         $this->assertEquals(15, $result);
+    }
+
+    static function dataProviderForMultipication(){
+        return [
+            [7,3,21],
+            [9,3,27],
+            [11,9,99],
+        ];
+    }
+
+    #[DataProvider('dataProviderForMultipication')]
+    // /** @dataProvider dataProviderForMultipication*/
+    function testMultipicationOfTwoNumbersUsingDataProvider($num1, $num2, $expected)
+    {
+        $result = $this->calc->multiply($num1, $num2);
+        $this->assertEquals($expected,$result);
     }
 }
